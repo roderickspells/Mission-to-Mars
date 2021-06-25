f6a6d916a7ec17684b45f966da1253c1f08ce126
roderickspells <59187034+roderickspells@users.noreply.github.com>
2021-06-24 19:25:36 -0500

    added completed deliverables 2 and 3

--- scraping.py
+++ scraping.py
@@ -22,8 +22,9 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
-    }
+        "last_modified": dt.datetime.now(),
+       "hemispheres": mars_hemispheres(browser)
+        }
 
     # Stop webdriver and return data
     browser.quit()
@@ -100,6 +101,27 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+
+def mars_hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+# 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    for i in range(4):
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.links.find_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+# 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
+    
 if __name__ == "__main__":
 
     # If running as script, print scraped data
